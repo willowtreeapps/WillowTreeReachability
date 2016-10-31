ae88d2318c1f8be10c6aeaf5f6a08ce7d898d22e
Michael Thomas <michael.thomas@willowtreeapps.com>
2016-10-31 10:52:10 -0400

    Fix SWIFT_VERSION build property & set Podspec tag to 2.0.1

--- Example/Pods/Target Support Files/WillowTreeReachability/WillowTreeReachability-umbrella.h
+++ Example/Pods/Target Support Files/WillowTreeReachability/WillowTreeReachability-umbrella.h
@@ -2,7 +2,6 @@
 #import <UIKit/UIKit.h>
 #endif
 
-#import "Reachability.h"
 
 FOUNDATION_EXPORT double WillowTreeReachabilityVersionNumber;
 FOUNDATION_EXPORT const unsigned char WillowTreeReachabilityVersionString[];
