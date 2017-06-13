0312f54d52f3f53c10ecc6d3064eb6b9934976f7
Erik LaManna <erik.lamanna@willowtreeapps.com>
2017-06-13 11:18:28 -0400

    Add tvOS scheme for Carthage support

---
+++ WillowTreeReachabilitytvOS/WillowTreeReachabilitytvOS.h
@@ -0,0 +1,19 @@
+//
+//  WillowTreeReachabilitytvOS.h
+//  WillowTreeReachabilitytvOS
+//
+//  Created by Erik LaManna on 6/13/17.
+//  Copyright © 2017 WillowTree, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WillowTreeReachabilitytvOS.
+FOUNDATION_EXPORT double WillowTreeReachabilitytvOSVersionNumber;
+
+//! Project version string for WillowTreeReachabilitytvOS.
+FOUNDATION_EXPORT const unsigned char WillowTreeReachabilitytvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WillowTreeReachabilitytvOS/PublicHeader.h>
+
+
